45b222618da82204b3423e0c154a287b854647c9
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2021-01-18 23:47:42 +0000

    Sorting algorithm made. prepareData method complete

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,4 @@
 // Please implement your solution in this file
-import LaunchesAPI from './spaceX_API';
 
 const prepareData = payload => {
   
@@ -10,7 +9,28 @@ const prepareData = payload => {
       launch.rocket.second_stage.payloads.some(obj => obj.customers.some(customer => customer.includes("NASA")))
     )
   });
-
+  
+  //Sort by  inverse chronological order with the exception that those that carried more payloads
+  // that should appear first.
+  preparedData.sort((a, b) => {
+    //1st Priority - Descendent order of number of payloads
+    if(a.rocket.second_stage.payloads.length < b.rocket.second_stage.payloads.length){
+      return 1;
+    }
+    if(b.rocket.second_stage.payloads.length < a.rocket.second_stage.payloads.length){
+      return -1;
+    }
+    //2nd Priority - for the ones that have the same number of payloads, 
+    // inverse chronological order
+    if(a.launch_date_utc < b.launch_date_utc){
+      return 1;
+    }
+    if(b.launch_date_utc < a.launch_date_utc){
+      return -1;
+    }
+    return 0;
+  })
+  
   //Transform the objects to get only the desired properties
   preparedData = preparedData.map(launch => {
     return {
