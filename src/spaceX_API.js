db658153113389cd5bd3c4f6cdfdc6db74371af1
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2021-01-18 18:25:26 +0000

    Added spaceX_API.js file to make the http request to spaceX API

---
+++ src/spaceX_API.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_BASE_ADDRESS = "https://api.spacexdata.com/v3/launches/past";
+
+export default class LaunchesAPI {
+  /*---HTTP REQUESTS-------------------------------------*/
+
+  /**GET /launches/past
+   * Get Past Launches from SpaceX API
+   */
+  static getPastLaunchesHttpRequest() {
+    const url = `${API_BASE_ADDRESS}/launches/past`;
+
+    return axios.get(url);
+  }
+}
