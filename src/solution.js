6d4b26d1fc4fd003f33324305a84d8ac9d5b866e
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2021-01-18 21:44:55 +0000

    filter objects that have at least one payload whose customer is NASA

--- src/solution.js
+++ src/solution.js
@@ -3,6 +3,13 @@ import LaunchesAPI from './spaceX_API';
 
 const prepareData = payload => {
   
+  //filter objects that have at least one payload whose customer is NASA
+  let preparedData = payload.filter(launch => {
+    return (
+      launch.launch_year === "2018" &&
+      launch.rocket.second_stage.payloads.some(obj => obj.customers.some(customer => customer.includes("NASA")))
+    )
+  });
 };
 
 const renderData = data => {
